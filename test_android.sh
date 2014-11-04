058d69c647fcc4b67a9883fe4a72ff5e8bec4c00
Andrey Lesnikov <ozkriff@gmail.com>
2014-11-05 00:13:32 +0400

    Simplified and renamed android.sh

---
+++ test_android.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+APPNAME=com.example.native_activity/android.app.NativeActivity
+
+cargo build --target arm-linux-androideabi -j 1 -v --release
+cp target/arm-linux-androideabi/release/libmarauder-*.a android/jni/librust.a
+cd android
+ndk-build
+ant debug
+adb install -r bin/RustyCardboard-debug.apk
+adb shell am start -n $APPNAME
+adb logcat | grep 'RustAndroidGlue\|native-activity'
+
