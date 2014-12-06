09def514ee4c926d05a3050b9154d6fc7505a6e5
Andrey Lesnikov <ozkriff@gmail.com>
2014-12-06 18:11:42 +0400

    Moved Visualizer to src/visualizer.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,13 @@
 #![feature(phase)]
 
-extern crate libc;
-
 #[cfg(target_os = "android")]
 #[phase(plugin, link)]
 extern crate android_glue;
 
-#[phase(plugin)]
-extern crate gl_generator;
-
-extern crate glutin;
-
-use glutin::{Event, VirtualKeyCode};
-
-mod gl {
-    generate_gl_bindings! {
-        api: "gles2",
-        profile: "core",
-        version: "2.0",
-        generator: "static_struct",
-    }
-}
-
-static mut COLOR_COUNTER: i32 = 0; // TODO
-
-struct Visualizer {
-    gl: gl::Gles2,
-    window: glutin::Window,
-    should_close: bool,
-}
-
-impl Visualizer {
-    fn new() -> Visualizer {
-        let window = glutin::Window::new().unwrap();
-        unsafe {
-            window.make_current();
-        };
-        let gl = gl::Gles2::load_with(|s| window.get_proc_address(s));
-        Visualizer {
-            gl: gl,
-            window: window,
-            should_close: false,
-        }
-    }
-
-    fn is_running(&self) -> bool {
-        !self.should_close
-    }
-
-    fn handle_events(&mut self) {
-        let events = self.window.poll_events().collect::<Vec<_>>();
-        if !events.is_empty() {
-            println!("{}", events);
-        }
-        for event in events.iter() {
-            match *event {
-                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape))
-                    | Event::Closed =>
-                {
-                    self.should_close = true;
-                },
-                _ => {},
-            }
-        }
-    }
-
-    fn draw(&self) {
-        unsafe {
-            match COLOR_COUNTER {
-                0 => self.gl.ClearColor(0.3, 0.0, 0.0, 1.0),
-                30 => self.gl.ClearColor(0.0, 0.3, 0.0, 1.0),
-                60 => self.gl.ClearColor(0.0, 0.0, 0.3, 1.0),
-                _ => if COLOR_COUNTER > 90 { COLOR_COUNTER = -1; }
-            }
-            COLOR_COUNTER += 1;
-            assert!(self.gl.GetError() == 0);
-            self.gl.Clear(gl::COLOR_BUFFER_BIT);
-        }
-        self.window.swap_buffers();
-    }
-
-    fn tick(&mut self) {
-        self.handle_events();
-        // self.logic();
-        self.draw();
-        // self.update_time();
-    }
-}
+mod visualizer;
 
 pub fn main() {
-    let mut visualizer = Visualizer::new();
+    let mut visualizer = visualizer::Visualizer::new();
     while visualizer.is_running() {
         visualizer.tick();
     }
---
+++ src/visualizer.rs
@@ -0,0 +1,85 @@
+extern crate libc;
+
+#[phase(plugin)]
+extern crate gl_generator;
+
+extern crate glutin;
+
+use self::glutin::{Event, VirtualKeyCode}; // TODO: why 'self'?
+
+mod gl {
+    generate_gl_bindings! {
+        api: "gles2",
+        profile: "core",
+        version: "2.0",
+        generator: "static_struct",
+    }
+}
+
+static mut COLOR_COUNTER: i32 = 0; // TODO
+
+pub struct Visualizer {
+    gl: gl::Gles2,
+    window: glutin::Window,
+    should_close: bool,
+}
+
+impl Visualizer {
+    pub fn new() -> Visualizer {
+        let window = glutin::Window::new().unwrap();
+        unsafe {
+            window.make_current();
+        };
+        let gl = gl::Gles2::load_with(|s| window.get_proc_address(s));
+        Visualizer {
+            gl: gl,
+            window: window,
+            should_close: false,
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        !self.should_close
+    }
+
+    fn handle_events(&mut self) {
+        let events = self.window.poll_events().collect::<Vec<_>>();
+        if !events.is_empty() {
+            println!("{}", events);
+        }
+        for event in events.iter() {
+            match *event {
+                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape))
+                    | Event::Closed =>
+                {
+                    self.should_close = true;
+                },
+                _ => {},
+            }
+        }
+    }
+
+    fn draw(&self) {
+        unsafe {
+            match COLOR_COUNTER {
+                0 => self.gl.ClearColor(0.3, 0.0, 0.0, 1.0),
+                30 => self.gl.ClearColor(0.0, 0.3, 0.0, 1.0),
+                60 => self.gl.ClearColor(0.0, 0.0, 0.3, 1.0),
+                _ => if COLOR_COUNTER > 90 { COLOR_COUNTER = -1; }
+            }
+            COLOR_COUNTER += 1;
+            assert!(self.gl.GetError() == 0);
+            self.gl.Clear(gl::COLOR_BUFFER_BIT);
+        }
+        self.window.swap_buffers();
+    }
+
+    pub fn tick(&mut self) {
+        self.handle_events();
+        // self.logic();
+        self.draw();
+        // self.update_time();
+    }
+}
+
+// vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
