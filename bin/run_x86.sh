26b57db1ece8ae42f26703efee8b4e932877dcbc
Andrey Lesnikov <ozkriff@gmail.com>
2014-12-27 14:09:35 +0300

    run_x86.sh: verbose, bt, -j 1

--- bin/run_x86.sh
+++ bin/run_x86.sh
@@ -1,2 +1,2 @@
 sed -i 's/^crate-type/# crate-type/' ../Cargo.toml
-cargo run; STATUS=$?; sed -i 's/^# crate-type/crate-type/' ../Cargo.toml; exit ${STATUS}
+RUST_BACKTRACE=1 cargo run --verbose -j 1; STATUS=$?; sed -i 's/^# crate-type/crate-type/' ../Cargo.toml; exit ${STATUS}
