b355610e0c358709b2ad42b9801e6337f072028a
Andrey Lesnikov <ozkriff@gmail.com>
2014-12-30 01:17:42 +0300

    marauder -> Zone of Control

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,8 +1,8 @@
 
-extern crate marauder;
+extern crate zoc;
 
 fn main() {
-    marauder::main();
+    zoc::main();
 }
 
 // vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
--- test_android.sh
+++ test_android.sh
@@ -5,7 +5,7 @@ set -e
 APPNAME=com.example.native_activity/android.app.NativeActivity
 
 cargo build --target arm-linux-androideabi -j 1 -v --release
-cp target/arm-linux-androideabi/release/libmarauder-*.a android/jni/librust.a
+cp target/arm-linux-androideabi/release/libzoc-*.a android/jni/librust.a
 cd android
 ndk-build
 ant debug
