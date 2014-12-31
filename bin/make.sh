1e507bf7138ed28c1ef20611ee12d5423f54db5d
Andrey Lesnikov <ozkriff@gmail.com>
2014-12-31 14:48:22 +0300

    bin/make.sh: Added link to cargo issue

--- bin/make.sh
+++ bin/make.sh
@@ -1,2 +1,3 @@
+# see https://github.com/rust-lang/cargo/issues/909
 sed -i 's/^crate-type/# crate-type/' ../Cargo.toml
 cargo build --verbose -j 1; STATUS=$?; sed -i 's/^# crate-type/crate-type/' ../Cargo.toml; exit ${STATUS}
