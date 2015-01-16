5dc3e1b5de0e0bd392c0bf5021ea40144445e1f3
Andrey Lesnikov <ozkriff@gmail.com>
2015-01-16 09:52:29 +0300

    visualizer mod: Export only 'Visualizer' class

--- src/visualizer/mod.rs
+++ src/visualizer/mod.rs
@@ -2,16 +2,16 @@
 
 pub use visualizer::visualizer::{Visualizer};
 
-pub mod mesh;
-pub mod geom;
-pub mod camera;
-pub mod visualizer;
-pub mod types;
-pub mod shader;
-pub mod zgl; // TODO: not pub
-pub mod picker;
-pub mod texture;
-pub mod obj;
-pub mod font_stash;
+mod mesh;
+mod geom;
+mod camera;
+mod visualizer;
+mod types;
+mod shader;
+mod zgl;
+mod picker;
+mod texture;
+mod obj;
+mod font_stash;
 
 // vim: set tabstop=5 shiftwidth=4 softtabstop=4 expandtab:
