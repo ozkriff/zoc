4e4ef75c75952566271144cdca32ba7aa4e0edb5
Andrey Lesnikov <ozkriff@gmail.com>
2015-03-07 16:37:39 +0300

    common/misc.rs: Removed commented-out 'read_file' func

--- src/common/src/misc.rs
+++ src/common/src/misc.rs
@@ -1,8 +1,6 @@
 // See LICENSE file for copyright and license details.
 
 use std::cmp;
-// use std::io::File;
-// use std::io::fs::PathExtensions;
 
 pub fn clamp<T>(n: T, min: T, max: T) -> T
     where T: Copy + cmp::PartialOrd
@@ -13,23 +11,6 @@ pub fn clamp<T>(n: T, min: T, max: T) -> T
         n => n,
     }
 }
-
-// TODO: Delete this
-/*
-pub fn read_file(path: &Path) -> String {
-    if !path.exists() {
-        panic!("Path does not exists: {}", path.display());
-    }
-    let bytes = match File::open(path).read_to_end() {
-        Ok(bytes) => bytes,
-        Err(msg) => panic!("Can not read from file {}: {}", path.display(), msg),
-    };
-    match String::from_utf8(bytes) {
-        Ok(s) => s,
-        Err(msg) => panic!("Not valid utf8 in file {}: {}", path.display(), msg),
-    }
-}
-*/
 
 pub fn add_quad_to_vec<T: Clone>(v: &mut Vec<T>, v1: T, v2: T, v3: T, v4: T) {
     v.push(v1.clone());
