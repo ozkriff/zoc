1a5da549962b3da8d5a309ba3adcb0f2e7aac395
Andrey Lesnikov <ozkriff@gmail.com>
2015-10-27 22:36:16 +0300

    zoc_gl/build.rs: PathBuf -> Path

--- src/zoc_gl/build.rs
+++ src/zoc_gl/build.rs
@@ -3,7 +3,7 @@
 extern crate gl_generator;
 extern crate khronos_api;
 
-use std::path::{PathBuf};
+use std::path::{Path};
 
 #[cfg(not(target_os = "android"))]
 const GENERATOR: gl_generator::StructGenerator = gl_generator::StructGenerator;
@@ -14,7 +14,7 @@ const GENERATOR: gl_generator::StaticStructGenerator = gl_generator::StaticStruc
 fn main() {
     let out_dir = std::env::var("OUT_DIR")
         .ok().expect("Can`t read OUT_DIR env var");
-    let dest = PathBuf::from(&out_dir);
+    let dest = Path::new(&out_dir);
     let mut file = match std::fs::File::create(&dest.join("gl_bindings.rs")) {
         Ok(file) => file,
         Err(err) => panic!("Can`t create 'gl_bindings.rs' file: {}", err),
