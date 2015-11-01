8fd85789e0b1353c0e3205f03efbfbc77fa4ef0e
Andrey Lesnikov <ozkriff@gmail.com>
2015-11-01 10:41:17 +0300

    Closed #121: common/fs: Add more info to error messages

--- src/common/src/fs.rs
+++ src/common/src/fs.rs
@@ -9,10 +9,19 @@ pub fn load<P: AsRef<Path>>(path: P) -> Cursor<Vec<u8>> {
     use std::io::{Read};
 
     let mut buf = Vec::new();
-    let mut file = File::open(&Path::new("assets").join(&path))
-        .expect("Can`t open file");
-    file.read_to_end(&mut buf).expect("Can`t open file");
-    Cursor::new(buf)
+    let fullpath = &Path::new("assets").join(&path);
+    let mut file = match File::open(&fullpath) {
+        Ok(file) => file,
+        Err(err) => {
+            panic!("Can`t open file '{}' ({})", fullpath.display(), err);
+        },
+    };
+    match file.read_to_end(&mut buf) {
+        Ok(_) => Cursor::new(buf),
+        Err(err) => {
+            panic!("Can`t read file '{}' ({})", fullpath.display(), err);
+        },
+    }
 }
 
 #[cfg(target_os = "android")]
@@ -21,9 +30,11 @@ pub fn load<P: AsRef<Path>>(path: P) -> Cursor<Vec<u8>> {
 
     let filename = path.as_ref().to_str()
         .expect("Can`t convert Path to &str");
-    let buf = android_glue::load_asset(filename)
-        .expect("Can`t load asset");
-    Cursor::new(buf)
+    match android_glue::load_asset(filename) {
+        Ok(buf) => Cursor::new(buf),
+        // TODO: more info about error
+        Err(_) => panic!("Can`t load asset '{}'", filename),
+    }
 }
 
 // vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
