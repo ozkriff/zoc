5cf50b93e57d188e083353a35ab2d9dd53da31f7
Andrey Lesnikov <ozkriff@gmail.com>
2016-08-24 18:55:36 +0300

    Closed #203: Merged 'visualizer' subcrate into main 'zoc' crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,45 @@
-extern crate visualizer;
+#[cfg(target_os = "android")]
+#[macro_use]
+extern crate android_glue;
+
+#[macro_use]
+extern crate gfx;
+
+extern crate gfx_window_glutin as gfx_glutin;
+extern crate gfx_device_gl as gfx_gl;
+extern crate rand;
+extern crate time;
+extern crate cgmath;
+extern crate collision;
+extern crate glutin;
+extern crate core;
+extern crate image;
+extern crate rusttype;
+
+mod visualizer;
+mod gui;
+mod obj;
+mod scene;
+mod event_visualizer;
+mod unit_type_visual_info;
+mod selection;
+mod types;
+mod pipeline;
+mod map_text;
+mod move_helper;
+mod camera;
+mod geom;
+mod screen;
+mod texture;
+mod tactical_screen;
+mod context_menu_popup;
+mod main_menu_screen;
+mod end_turn_screen;
+mod game_results_screen;
+mod context;
+mod text;
+mod mesh;
+mod fs;
 
 use visualizer::{Visualizer};
 
--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,48 +1,8 @@
-#[cfg(target_os = "android")]
-#[macro_use]
-extern crate android_glue;
-
-#[macro_use]
-extern crate gfx;
-
-extern crate gfx_window_glutin as gfx_glutin;
-extern crate gfx_device_gl as gfx_gl;
-extern crate rand;
-extern crate time;
-extern crate cgmath;
-extern crate collision;
-extern crate glutin;
-extern crate core;
-extern crate image;
-extern crate rusttype;
-
-mod gui;
-mod obj;
-mod scene;
-mod event_visualizer;
-mod unit_type_visual_info;
-mod selection;
-mod types;
-mod pipeline;
-mod map_text;
-mod move_helper;
-mod camera;
-mod geom;
-mod screen;
-mod texture;
-mod tactical_screen;
-mod context_menu_popup;
-mod main_menu_screen;
-mod end_turn_screen;
-mod game_results_screen;
-mod context;
-mod text;
-mod mesh;
-mod fs;
-
+use std::process;
 use std::sync::mpsc::{channel, Receiver};
 use std::fs::{metadata};
 use gfx::traits::{Device};
+use time;
 use screen::{Screen, ScreenCommand, EventStatus};
 use context::{Context};
 use main_menu_screen::{MainMenuScreen};
@@ -52,7 +12,7 @@ fn check_assets_dir() {
     if let Err(e) = metadata("assets") {
         println!("Can`t find 'assets' dir: {}", e);
         println!("Note: see 'Assets' section of Readme.rst");
-        std::process::exit(1);
+        process::exit(1);
     }
 }
 
