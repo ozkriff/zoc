b51ffef453c4d6ca26e5317e18aa8a89230538ba
Andrey Lesnikov <ozkriff@gmail.com>
2016-09-16 22:43:10 +0300

    Added test for 'clamp' func

--- core/src/misc.rs
+++ core/src/misc.rs
@@ -10,3 +10,17 @@ pub fn clamp<T>(n: T, min: T, max: T) -> T
         n => n,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use misc::{clamp};
+
+    #[test]
+    fn test_clamp() {
+        assert_eq!(clamp(-1, 0, 10), 0);
+        assert_eq!(clamp(11, 0, 10), 10);
+        assert_eq!(clamp(0, 0, 10), 0);
+        assert_eq!(clamp(10, 0, 10), 10);
+        assert_eq!(clamp(5, 0, 10), 5);
+    }
+}
