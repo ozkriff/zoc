29d36a8f47628fe78aaac93f787bbb6d7f78bc57
Andrey Lesnikov <ozkriff@gmail.com>
2017-02-06 12:36:23 +0300

    fov.rs: Added test for #149

--- core/src/fov.rs
+++ core/src/fov.rs
@@ -84,3 +84,48 @@ pub fn simple_fov(
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use cgmath::{Vector2};
+    use std::rc::{Rc};
+    use game_state::{State};
+    use db::{Db};
+    use fov::{fov};
+    use ::{MapPos, Options, GameType, Distance};
+
+    #[test]
+    fn test_bug_149_fov_is_asymmetric() {
+        let db = Rc::new(Db::new());
+        let options = Options {
+            game_type: GameType::Hotseat,
+            map_name: "map_fov_bug_test".into(),
+            players_count: 2,
+        };
+        let state = State::new_full(db, &options);
+        let range = Distance{n: 2};
+        let origin = MapPos { v: Vector2 { x: 10, y: 10 } };
+        let mut real = vec![];
+        fov(&state, origin, range, &mut |pos| real.push(pos));
+        let expected = vec![
+            MapPos { v: Vector2 {x: 10, y: 10} },
+
+            MapPos { v: Vector2 {x: 10, y: 9} }, // trees here
+            MapPos { v: Vector2 {x: 11, y: 9} },
+            MapPos { v: Vector2 {x: 11, y: 10} },
+            MapPos { v: Vector2 {x: 11, y: 11} },
+            MapPos { v: Vector2 {x: 10, y: 11} },
+            MapPos { v: Vector2 {x: 9, y: 10} }, // trees here
+
+            MapPos { v: Vector2 {x: 10, y: 8} }, // TODO: WTF?!
+            MapPos { v: Vector2 {x: 11, y: 8} },
+            MapPos { v: Vector2 {x: 12, y: 9} },
+            MapPos { v: Vector2 {x: 12, y: 10} },
+            MapPos { v: Vector2 {x: 12, y: 11} },
+            MapPos { v: Vector2 {x: 11, y: 12} },
+            MapPos { v: Vector2 {x: 10, y: 12} },
+            MapPos { v: Vector2 {x: 9, y: 12} }
+        ];
+        assert_eq!(real, expected);
+    }
+}
