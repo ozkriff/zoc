94055b0cd75310a374f5239068892a9fbda46ff9
Andrey Lesnikov <ozkriff@gmail.com>
2017-02-08 22:43:12 +0300

    Updated deps

--- src/texture.rs
+++ src/texture.rs
@@ -1,7 +1,7 @@
 use std::io::{Cursor};
 use image;
 use gfx::handle::{ShaderResourceView};
-use gfx::{self, tex};
+use gfx::{self, texture};
 use gfx_gl;
 use types::{Size2};
 use pipeline::{ColorFormat};
@@ -19,7 +19,7 @@ pub fn load_texture(context: &mut Context, data: &[u8]) -> ShaderResourceView<gf
 pub fn load_texture_raw<R, F>(factory: &mut F, size: Size2, data: &[u8]) -> ShaderResourceView<R, [f32; 4]>
     where R: gfx::Resources, F: gfx::Factory<R>
 {
-    let kind = tex::Kind::D2(size.w as tex::Size, size.h as tex::Size, tex::AaMode::Single);
-    let (_, view) = factory.create_texture_const_u8::<ColorFormat>(kind, &[data]).unwrap();
+    let kind = texture::Kind::D2(size.w as texture::Size, size.h as texture::Size, texture::AaMode::Single);
+    let (_, view) = factory.create_texture_immutable_u8::<ColorFormat>(kind, &[data]).unwrap();
     view
 }
