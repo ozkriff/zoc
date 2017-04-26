754c34dcfd710bdd7b5ca201c86836628c4caaa7
Andrey Lesnikov <ozkriff@gmail.com>
2017-04-26 21:36:16 +0300

    Fixed destruction of vehicles

--- src/action/create_unit.rs
+++ src/action/create_unit.rs
@@ -42,13 +42,6 @@ impl Action for CreateUnit {
         });
         set_children(context, self.node_id, &self.unit, mesh_id);
         if self.unit.is_alive {
-            let id = context.scene.allocate_node_id();
-            context.scene.set_child_node(self.node_id, id, SceneNode {
-                pos: WorldPos{v: geom::vec3_z(geom::HEX_EX_RADIUS / 2.0)},
-                mesh_id: Some(context.mesh_ids.marker_mesh_id),
-                color: gen::get_player_color(self.unit.player_id),
-                .. Default::default()
-            });
             let shadow_node_id = context.scene.allocate_node_id();
             context.scene.set_child_node(self.node_id, shadow_node_id, SceneNode {
                 pos: WorldPos{v: geom::vec3_z(0.01)},
@@ -56,6 +49,13 @@ impl Action for CreateUnit {
                 mesh_id: Some(context.mesh_ids.shadow_mesh_id),
                 color: [1.0, 0.0, 0.0, 0.8],
                 node_type: scene::SceneNodeType::Transparent,
+                .. Default::default()
+            });
+            let id = context.scene.allocate_node_id();
+            context.scene.set_child_node(self.node_id, id, SceneNode {
+                pos: WorldPos{v: geom::vec3_z(geom::HEX_EX_RADIUS / 2.0)},
+                mesh_id: Some(context.mesh_ids.marker_mesh_id),
+                color: gen::get_player_color(self.unit.player_id),
                 .. Default::default()
             });
         }
